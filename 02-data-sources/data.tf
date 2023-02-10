6a02ea48bf65bfd178ba5035111307d1053019fa
krishna <brkkrishna9@gmail.com>
2023-02-10 19:15:35 +0530

    new changes

---
+++ 02-data-sources/data.tf
@@ -0,0 +1,25 @@
+data "aws_ami" "centos8" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["718756811784"]
+}
+
+output "ami" {
+  value = data.aws_ami.centos8
+}
+
+output "public_ip" {
+  value = data.aws_instance.foo.public_ip
+}
+
+output "private_ip" {
+  value = data.aws_instance.foo.private_ip
+}
+
+data "aws_instance" "foo" {
+  instance_id = "i-0d5fc7fb99c768588"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
