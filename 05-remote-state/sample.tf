14894f6fc929f99490436e36f63fa160aee8fd93
krishna <brkkrishna9@gmail.com>
2023-02-15 21:18:52 +0530

    new changes

--- 05-remote-state/sample.tf
+++ 05-remote-state/sample.tf
@@ -1,21 +1,21 @@
-#provider "aws" {
-#  region = "us-east-1"
-#}
-#
-#data "aws_ami" "centos8" {
-#  most_recent = true
-#  name_regex  = "Centos-8-DevOps-Practice"
-#  owners      = ["973714476881"]
-#}
-#
-#resource "aws_instance" "web" {
-#  ami           = data.aws_ami.centos8.id
-#  instance_type = "t3.micro"
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "centos8" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.centos8.id
+  instance_type = "t3.micro"
 #  vpc_security_group_ids = [aws_security_group.allow_tls.id]
-#  tags = {
-#    Name = "test-centos8"
-#  }
-#}
+  tags = {
+    Name = "test-centos8"
+  }
+}
 
 terraform {
   backend "s3" {
